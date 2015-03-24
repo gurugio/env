1f4db4b964379d4ce2a20e08e43d9334e390aded
Gioh Kim <gurugio@hanmail.net>
2015-03-25 06:24:57 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -8,20 +8,6 @@ then
  cp -r * ~/env
 fi
 
-echo "Do you want to set ALTIBASE environment? [y/n]"
-read work
-
-echo "Building .bashrc and .bash_profile..."
-mv ${HOME}/.bashrc bashrc.bak
-
-if [ "$work" = "y" ]
-then
-    echo "source ${HOME}/env/bashrc_altibase" > ${HOME}/.bashrc
-else
-    echo "source ${HOME}/env/bashrc_home" > ${HOME}/.bashrc
-fi
-cp ${HOME}/.bashrc ${HOME}/.bash_profile
-
 echo "Building .vimrc and .gvimrc..."
 echo "source ${HOME}/env/vimrc" > ${HOME}/.vimrc
 
@@ -33,12 +19,4 @@ echo "Linking .emacs to env/emacs..."
 rm -rf ${HOME}/.emacs
 ln -s ${HOME}/env/emacs ${HOME}/.emacs
 
-echo "Linking .globalrc to env/gtags.conf"
-rm -rf ${HOME}/.globalrc
-ln -s ${HOME}/env/gtags.conf ${HOME}/.globalrc
 
-if [ "$work" = "n" ]
-then
-    sh kozip_install.sh
-    sudo cp svn_clean /usr/local/bin
-fi
